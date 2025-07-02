fea774839c035d0ef87e0a0b20de771f5b86ac29
rgautr01 <rgautr01@gmail.com>
2025-07-02 16:34:16 +0200

    refactor PokedexApplication to update base URL and add getPokemonSpecies method

--- src/applications/pokedex.application.js
+++ src/applications/pokedex.application.js
@@ -3,16 +3,27 @@ const Pokemon = require('../models/Pokemon');
 
 class PokedexApplication {
     constructor() {
-        this.baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+        this.baseUrl = 'https://pokeapi.co/api/v2';
     }
 
     async getPokemon(pokemonName) {
-        const normalizedName = pokemonName.toLowerCase().trim();
-
         try {
-            const response = await axios.get(`${this.baseUrl}${normalizedName}`);
+            const response = await axios.get(`${this.baseUrl}/pokemon/${pokemonName}`);
             
             return new Pokemon(response.data);
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+                throw new Error(`Pokemon "${pokemonName}" not found`);
+            }
+            throw new Error(`Error fetching Pokemon: ${error.message}`);
+        }
+    }
+    
+    async getPokemonSpecies(pokemonName){
+        try {
+            const response = await axios.get(`${this.baseUrl}/pokemon-species/${pokemonName}`);
+            
+            return new PokemonSpecies(response.data);
         } catch (error) {
             if (error.response && error.response.status === 404) {
                 throw new Error(`Pokemon "${pokemonName}" not found`);
